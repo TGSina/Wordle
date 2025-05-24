f39a25074e09c7f93a236260a9d474d6bc36b067
Sina Yarmohammadi <sinayar09mohammadi90@gmail.com>
2025-05-24 09:16:48 +0330

    Starting to develop Persian Wordle.
    Created genereate_word_freq_fa function in test.ipynb and then added to
    utils.py.
    persian-wikipedia.txt and riran-wikipedia.txt can be read now.
    
    data.json deleted as it was not needed anymore.
    some files and directories got organized.

--- src/utils.py
+++ src/utils.py
@@ -24,19 +24,49 @@ def generate_word_freq(file_path, word_count: int = 5, limit: int = 1000):
 
 
     # Filtering data with word_count:
-
     words_list_freq = list(filter(lambda w: len(w[0]) == word_count, words_list_freq))
 
     # Sorting data with limit:
-
     words_list_freq = sorted(words_list_freq, key=lambda w: w[1], reverse=True)
 
     # Limit data:
-
     words_list_freq = words_list_freq[:limit]
 
     # Drop frequency of words data
-
     words = [word[0] for word in words_list_freq ]
 
     return words
+
+
+def generate_word_freq_fa(file_path, word_count: int = 5, limit: int = 1000, ignore_lines: int = 20):
+    """
+    Generates Persian words from a given .txt file, based on words frequencies.
+    """
+
+    # Extract words and their frequencies in a list of tuples:
+    with open(file_path) as file:
+        counter = 0
+        faword_freq_list = []
+        
+        for line in file:
+            counter += 1
+            if counter <= ignore_lines:
+                continue
+    
+            faword, freq = line.strip().split()
+            freq = int(freq)
+            faword_freq_list.append((faword, freq))
+
+    # Filtering based on word_count
+    faword_freq_list = list(filter(lambda w: len(w[0]) == word_count, faword_freq_list))
+
+    # Sorting words based on frequencies
+    faword_freq_list = sorted(faword_freq_list, key=lambda w: w[1], reverse=True)
+
+    # Keep the most common words with limit
+    faword_freq_list = faword_freq_list[0:limit]
+
+    # Drop frequencies and return the words list
+    fa_words = [w[0] for w in faword_freq_list]
+
+    return fa_words
